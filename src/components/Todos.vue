d651d24f1b3f3d5ee11163f21dbb127db79fe2ae
MikiTran <mikitran721@gmail.com>
2023-11-17 16:16:25 +0700

    su dung props - truyen data giua cac components

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,12 +1,14 @@
 <template>
-  <p v-for="todo in todos" v-bind:key="todo">{{ todo }}</p>
+  <TodoItem v-for="todo in todos" v-bind:key="todo" v-bind:todoProps="todo" />
 </template>
 
 <script>
 import { ref } from 'vue'; //su dung khoi diem
+import TodoItem from './TodoItem'
 
 export default {
     name:'TodosApp',
+    components:{TodoItem},
     setup(){
         //khai bao du lieu khoi diem
         const todos=ref(['Viec 1','Viec 2','Viec 3'])
