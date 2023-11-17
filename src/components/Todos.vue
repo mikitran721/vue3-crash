870375965e124846e2a0055d9c8d22eaeb8508cc
MikiTran <mikitran721@gmail.com>
2023-11-17 15:57:30 +0700

    trang thai khoi diem cua component

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,14 +1,21 @@
 <template>
-  <p>
-    Viec 1
-  </p>
-  <p>Viec 2</p>
-  <p>Viec 3</p>
+  <p v-for="todo in todos" v-bind:key="todo">{{ todo }}</p>
 </template>
 
 <script>
+import { ref } from 'vue'; //su dung khoi diem
+
 export default {
-    name:'TodosApp'
+    name:'TodosApp',
+    setup(){
+        //khai bao du lieu khoi diem
+        const todos=ref(['Viec 1','Viec 2','Viec 3'])
+
+        //xuat khau data khai bao ben tren
+        return {
+            todos
+        }
+    }
 }
 </script>
 
